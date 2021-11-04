153de23fe01a632e873e58bfc3d015f6ac317233
Meifa Tang <tmf@33.cn>
2021-11-04 17:53:54 +0800

    tnux:chain33 ADD

---
+++ tnux/packages/chain33.scm
@@ -0,0 +1,23 @@
+(define-module (tnux packages chain33)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix licenses))
+
+(define-public chain33
+  (package
+   (name "chain33")
+   (version "1.65.5")
+   (source (origin
+            (method git-fetch)
+            (uri (git-reference
+                  (url "https://github.com/33cn/chain33")
+                  (commit version)
+            (sha256
+             (base32
+              "0nfr0ivh6vx43bxf4d7sb2anwb0r22i8hkb4vldjd99d8gslp46h"))))))
+   (build-system gnu-build-system)
+   (home-page "https://github.com/33cn/chain33")
+   (synopsis "高度模块化, 遵循 KISS原则的区块链开发框架")
+   (description "高度模块化, 遵循 KISS原则的区块链开发框架")
+   (license bsd-3)))
