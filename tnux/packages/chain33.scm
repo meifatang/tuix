ceb4a94deaddf8d0368fc04ea1e8c9f799c9a403
Meifa Tang <tmf@33.cn>
2021-11-04 18:21:01 +0800

    tnux:chain33 FIX

--- tnux/packages/chain33.scm
+++ tnux/packages/chain33.scm
@@ -8,17 +8,18 @@
   (package
    (name "chain33")
    (version "1.65.5")
-   (source (origin
-            (method git-fetch)
-            (uri (git-reference
-                  (url "https://github.com/33cn/chain33")
-                  (commit (string-append "v" version))))
-            (file-name (git-file-name name version))
-            (sha256
-             (base32
-              "1wlv7ckl6qdcwbrgx5kjw7f7jncb828b2fgiwm65rpcrcjm0anzs"))))
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+           (url "https://github.com/33cn/chain33")
+           (commit (string-append "v" version))))
+     (file-name (git-file-name name version))
+     (sha256
+      (base32
+       "1wlv7ckl6qdcwbrgx5kjw7f7jncb828b2fgiwm65rpcrcjm0anzs"))))
    (build-system gnu-build-system)
    (home-page "https://github.com/33cn/chain33")
-   (synopsis "高度模块化, 遵循 KISS原则的区块链开发框架")
-   (description "高度模块化, 遵循 KISS原则的区块链开发框架")
+   (synopsis "chain33")
+   (description "chain33")
    (license bsd-3)))
