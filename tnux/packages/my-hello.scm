9f412af9db8aea243d40939dac9f4aa2327b0eda
Meifa Tang <tmf@33.cn>
2021-11-04 14:40:21 +0800

    tnux edit define-public

--- tnux/packages/my-hello.scm
+++ tnux/packages/my-hello.scm
@@ -4,21 +4,22 @@
   #:use-module (guix build-system gnu)
   #:use-module (guix licenses))
 
-(package
-  (name "my-hello")
-  (version "2.10")
-  (source (origin
+(define-public my-hello
+  (package
+   (name "my-hello")
+   (version "2.10")
+   (source (origin
             (method url-fetch)
             (uri (string-append "mirror://gnu/hello/hello-" version
                                 ".tar.gz"))
             (sha256
              (base32
               "0ssi1wpaf7plaswqqjwigppsg5fyh99vdlb9kzl7c9lng89ndq1i"))))
-  (build-system gnu-build-system)
-  (synopsis "Hello, Guix world: An example custom Guix package")
-  (description
-   "GNU Hello prints the message \"Hello, world!\" and then exits.  It
+   (build-system gnu-build-system)
+   (synopsis "Hello, Guix world: An example custom Guix package")
+   (description
+    "GNU Hello prints the message \"Hello, world!\" and then exits.  It
 serves as an example of standard GNU coding practices.  As such, it supports
 command-line arguments, multiple languages, and so on.")
-  (home-page "https://www.gnu.org/software/hello/")
-  (license gpl3+))
+   (home-page "https://www.gnu.org/software/hello/")
+   (license gpl3+)))
